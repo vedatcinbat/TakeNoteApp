b5af02fb9fa304106e86760ced5d1eca447d105c
vedatcinbat <73201483+vedatcinbat@users.noreply.github.com>
2024-01-08 23:58:52 +0300

    Update ready-database.php

--- includes/ready-database.php
+++ includes/ready-database.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "takingnoteapp";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
